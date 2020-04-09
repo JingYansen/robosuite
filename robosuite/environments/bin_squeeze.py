1. Change action[3:7] as value( not delta) 2. Change penalty func 3. Not fix rotation

robosuite/environments/bin_squeeze.py
@@ -92,6 +92,7 @@ class BinSqueeze(SawyerEnv, mujoco_env.MujocoEnv):
                 ],
             }],
             fix_rotation=False,
+            no_delta=False,
     ):
         """
         Args:
@@ -181,6 +182,7 @@ class BinSqueeze(SawyerEnv, mujoco_env.MujocoEnv):
         self.test_cases = test_cases
         self.initialize_objects = False
         self.fix_rotation = fix_rotation
+        self.no_delta = no_delta
         if self.fix_rotation:
             self.action_dim = 3
         else:
@@ -497,7 +499,12 @@ class BinSqueeze(SawyerEnv, mujoco_env.MujocoEnv):
         delta_X, delta_Y, delta_Z = action[4:7].copy()
         angle = self._angle(np.array([X, Y, Z]), np.array([delta_X, delta_Y, delta_Z]))
 
-        new_X, new_Y, new_Z = self._normalize(np.array([X + delta_X, Y + delta_Y, Z + delta_Z]))
+        # (u, v, w, t) is world rotation
+        if self.no_delta:
+            new_X, new_Y, new_Z = self._normalize(np.array([delta_X, delta_Y, delta_Z]))
+        # (u, v, w, t) is the delta rotation
+        else:
+            new_X, new_Y, new_Z = self._normalize(np.array([X + delta_X, Y + delta_Y, Z + delta_Z]))
 
         new_theta = theta + action[3].copy()
         if new_theta >= 2 * np.pi and action[3] > 0:
@@ -631,7 +638,7 @@ class BinSqueeze(SawyerEnv, mujoco_env.MujocoEnv):
             self.prepare_objects()
 
         ## pre action: remove gravity and other forces.
-        info.update({'theta': self._sigmoid(action[3].copy()), 'old_action': action.copy()})
+        info.update({'theta': action[3].copy(), 'old_action': action.copy()})
         self._pre_action(action)
 
         ## get cur pos
@@ -707,7 +714,7 @@ class BinSqueeze(SawyerEnv, mujoco_env.MujocoEnv):
         z_pos = target_pos[2]
 
         # energy
-        energy = self.energy_tradeoff * (info['angle'] / 180 + info['theta'])
+        energy = self.energy_tradeoff * (info['angle'] / 180 + np.abs(info['theta']))
         assert energy <= self.energy_tradeoff * 2 and energy >= 0
 
 

robosuite/scripts/demo_bin_squeeze_test.py
@@ -73,10 +73,10 @@ def test_video(env, video_path='demo/test/test.mp4'):
     episodes = 1
     # action = env.action_space.sample()
     # action[0:3] = np.array([1., 0., 0.])
-    # action = np.array([0.4, 0.4, -1., 0., 0., 0., 0.])
-    left = np.array([1., 0., 0., 0., 0., 0., 0.])
-    front = np.array([0., 1., 0., 0., 0., 0., 0.])
-    up = np.array([0., 0., 1., 0., 0., 0., 0.])
+    # action = np.array([0, 0, 0., 1., 1., 1., 1.])
+    # left = np.array([1., 0., 0., 0., 0., 0., 0.])
+    # front = np.array([0., 1., 0., 0., 0., 0., 0.])
+    # up = np.array([0., 0., 1., 0., 0., 0., 0.])
     # down = np.array([0.1, 0., -1.])
     for _ in range(episodes):
         env.reset()
@@ -84,10 +84,11 @@ def test_video(env, video_path='demo/test/test.mp4'):
         arr_imgs = []
         succ = False
 
-        for i in range(200):
+        for i in range(100):
             # run a uniformly random agent
             action = env.action_space.sample()
             # action = up.copy()
+            action[0:3] = 0.
 
             obs, reward, done, info = env.step(action)
 
@@ -182,7 +183,7 @@ if __name__ == "__main__":
         camera_width=128,
         camera_depth=True,
         place_num=3,
-        # fix_rotation=True,
+        no_delta=True,
         test_cases=[],
     )
 